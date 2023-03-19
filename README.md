c3563390e7a88beacfe8158203f171a8b7d68cb7
CatYunnnn <marco20001114@gmail.com>
2023-03-19 15:24:19 +0800

    restaurant project practice

---
+++ README.md
@@ -0,0 +1,25 @@
+#打造餐廳清單
+
+使用 Node.js 、 Express-handlebars 等工具打造的美食網站練習
+
+#基本練習
+
+使用 express-handlebars 將原本靜態的網站變成動態渲染
+
+了解如何在 views 資料夾下引用 public 中的 js 以及 css 檔案
+
+了解並使用 params 以及 querystring 來完成動態餐廳詳細介紹渲染以及尋找餐廳的功能
+
+#流程
+
+1.建立 restaurant 資料夾
+
+2.載入 npm 、 express 、 express-handlebars 、 nodemon 等功能
+
+3.建立視覺模板 views
+
+4.載入 public 內容
+
+5.將 restaurant.json 中的資料動態渲染到網站上
+
+6.運用 params 以及 querystring 來完成餐廳的詳細資料以及餐廳的查詢功能
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const exphbs = require("express-handlebars");
+const resList = require("./restaurant.json");
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Index",
+    style: "res.css",
+    reses: resList.results,
+  });
+});
+
+app.get("/restaurants/:res_id", (req, res) => {
+  const resinfo = resList.results.filter((index) => {
+    return index.id == req.params.res_id;
+  });
+  res.render("show", { title: "Show", style: "res1.css", resinfo: resinfo });
+});
+
+app.get("/search", (req, res) => {
+  console.log("req", req.query.yoursearch);
+  const results = resList.results.filter((movie) => {
+    return movie.name
+      .toLowerCase()
+      .includes(req.query.yoursearch.toLowerCase());
+  });
+  res.render("index", {
+    reses: results,
+    style: "res.css",
+    yoursearch: req.query.yoursearch,
+  });
+});
+app.listen(port, () => {
+  console.log("running running running~");
+});
